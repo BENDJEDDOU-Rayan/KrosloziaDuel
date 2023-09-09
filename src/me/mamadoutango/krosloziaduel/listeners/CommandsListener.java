5bd3e41eee82ebb681bc118ea1b64e86b0e23ccc
MamadouTango <rayanbendjeddou13@gmail.com>
2023-09-10 01:47:39 +0200

    Amélioration système blocage de commandes durant un duel

--- src/me/mamadoutango/krosloziaduel/listeners/CommandsListener.java
+++ src/me/mamadoutango/krosloziaduel/listeners/CommandsListener.java
@@ -10,57 +10,58 @@ public class CommandsListener implements Listener {
     @EventHandler
     public void onCommands(PlayerCommandPreprocessEvent e){
         if(DuelDataManager.getDuelEnCours().containsKey(e.getPlayer()) || DuelDataManager.getDuelEnCours().containsValue(e.getPlayer())){
+            String[] commande = e.getMessage().split(" ");
             if(e.getMessage().equalsIgnoreCase("/spawn")){
                 e.setCancelled(true);
-                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " &cpendant le duel !");
-            } else if(e.getMessage().equalsIgnoreCase("/tpa")){
+                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " §cpendant le duel !");
+            } else if(commande[0].equalsIgnoreCase("/tpa")){
                 e.setCancelled(true);
-                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " &cpendant le duel !");
-            } else if(e.getMessage().equalsIgnoreCase("/tpaccept")){
+                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " §cpendant le duel !");
+            } else if(commande[0].equalsIgnoreCase("/tpaccept")){
                 e.setCancelled(true);
-                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " &cpendant le duel !");
-            } else if(e.getMessage().equalsIgnoreCase("/tpdeny")){
+                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " §cpendant le duel !");
+            } else if(commande[0].equalsIgnoreCase("/tpdeny")){
                 e.setCancelled(true);
-                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " &cpendant le duel !");
-            } else if(e.getMessage().equalsIgnoreCase("/tpahere")){
+                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " §cpendant le duel !");
+            } else if(commande[0].equalsIgnoreCase("/tpahere")){
                 e.setCancelled(true);
-                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " &cpendant le duel !");
-            } else if(e.getMessage().equalsIgnoreCase("/tpyes")){
+                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " §cpendant le duel !");
+            } else if(commande[0].equalsIgnoreCase("/tpyes")){
                 e.setCancelled(true);
-                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " &cpendant le duel !");
-            } else if(e.getMessage().equalsIgnoreCase("/tpno")){
+                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " §cpendant le duel !");
+            } else if(commande[0].equalsIgnoreCase("/tpno")){
                 e.setCancelled(true);
-                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " &cpendant le duel !");
-            } else if(e.getMessage().equalsIgnoreCase("/home")){
+                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " §cpendant le duel !");
+            } else if(commande[0].equalsIgnoreCase("/home")){
                 e.setCancelled(true);
-                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " &cpendant le duel !");
-            } else if(e.getMessage().equalsIgnoreCase("/e:home")){
+                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " §cpendant le duel !");
+            } else if(commande[0].equalsIgnoreCase("/e:home")){
                 e.setCancelled(true);
-                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " &cpendant le duel !");
-            } else if(e.getMessage().equalsIgnoreCase("/ehome")){
+                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " §cpendant le duel !");
+            } else if(commande[0].equalsIgnoreCase("/ehome")){
                 e.setCancelled(true);
-                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " &cpendant le duel !");
-            } else if(e.getMessage().equalsIgnoreCase("/ec")){
+                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " §cpendant le duel !");
+            } else if(commande[0].equalsIgnoreCase("/ec")){
                 e.setCancelled(true);
-                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " &cpendant le duel !");
-            } else if(e.getMessage().equalsIgnoreCase("/enderchest")){
+                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " §cpendant le duel !");
+            } else if(commande[0].equalsIgnoreCase("/enderchest")){
                 e.setCancelled(true);
-                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " &cpendant le duel !");
-            } else if(e.getMessage().equalsIgnoreCase("/kit")){
+                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " §cpendant le duel !");
+            } else if(commande[0].equalsIgnoreCase("/kit")){
                 e.setCancelled(true);
-                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " &cpendant le duel !");
-            } else if(e.getMessage().equalsIgnoreCase("/feed")){
+                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " §cpendant le duel !");
+            } else if(commande[0].equalsIgnoreCase("/feed")){
                 e.setCancelled(true);
-                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " &cpendant le duel !");
-            } else if(e.getMessage().equalsIgnoreCase("/repair")){
+                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " §cpendant le duel !");
+            } else if(commande[0].equalsIgnoreCase("/repair")){
                 e.setCancelled(true);
-                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " &cpendant le duel !");
-            } else if(e.getMessage().equalsIgnoreCase("/rtp")){
+                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " §cpendant le duel !");
+            } else if(commande[0].equalsIgnoreCase("/rtp")){
                 e.setCancelled(true);
-                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " &cpendant le duel !");
-            } else if(e.getMessage().equalsIgnoreCase("/duel")){
+                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " §cpendant le duel !");
+            } else if(commande[0].equalsIgnoreCase("/duel")){
                 e.setCancelled(true);
-                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " &cpendant le duel !");
+                e.getPlayer().sendMessage("§eKroslozia §7» §cVous ne pouvez pas exécuté la commande §e" + e.getMessage() + " §cpendant le duel !");
             }
         }
     }
